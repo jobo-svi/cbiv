4a83b2f41b136e3e1c2385862b56535b3e83b3f6
Josh Bonnell <joshrbonnell@gmail.com>
2023-05-29 16:02:09 -0500

    I can see the light at the end of the tunnel

--- src/components/Droppable.js
+++ src/components/Droppable.js
@@ -2,26 +2,37 @@ import React from "react";
 import { useSortable } from "@dnd-kit/sortable";
 
 const Droppable = (props) => {
-    const {
-        attributes,
-        listeners,
-        setNodeRef,
-        transform,
-        transition,
-        isDragging,
-    } = useSortable({
-        id: props.id,
-        data: {
-            id: props.id,
-            rowIndex: props.rowIndex,
-        },
-    });
+  const {
+    activeIndex,
+    attributes,
+    listeners,
+    setNodeRef,
+    node,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: props.id,
+    data: {
+      id: props.id,
+      rowIndex: props.rowIndex,
+      relativePosition: props.relativePosition,
+    },
+  });
 
-    return (
-        <div className="grid-row" ref={setNodeRef}>
-            {props.children}
-        </div>
-    );
+  let height = null;
+  if (node.current && !props.isPlaceholder && props.activeId) {
+    height = node.current.clientHeight;
+  }
+  return (
+    <div
+      className="grid-row"
+      ref={setNodeRef}
+      style={{ height: height !== null ? `${height}px` : "" }}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default Droppable;
--- src/components/SortableItem.js
+++ src/components/SortableItem.js
@@ -1,58 +1,46 @@
 import React from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import { useDndContext } from "@dnd-kit/core";
 
 const SortableItem = (props) => {
-    const {
-        attributes,
-        listeners,
-        setNodeRef,
-        transform,
-        transition,
-        isDragging,
-    } = useSortable({
-        id: props.id,
-        data: {
-            id: props.id,
-            rowIndex: props.rowIndex,
-            //colIndex: props.colIndex,
-        },
-    });
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: props.id,
+    data: {
+      id: props.id,
+      rowIndex: props.rowIndex,
+      relativePosition: props.relativePosition,
+    },
+  });
 
-    //const { collisions } = useDndContext();
+  // We don't want anything to scale
+  if (transform) {
+    transform.scaleX = 1;
+    transform.scaleY = 1;
+  }
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    opacity: isDragging ? ".5" : 1,
+  };
 
-    // let isHovered = false;
-    // if (collisions) {
-    //     let c = collisions.find((collision) => collision.id === props.id);
-    //     if (c && c.data) {
-    //         isHovered = c.data.hovered;
-    //     }
-    // }
-
-    // // We don't want anything to scale
-    // if (transform) {
-    //     transform.scaleX = 1;
-    //     transform.scaleY = 1;
-    // }
-    const style = {
-        transform: CSS.Transform.toString(transform),
-        transition,
-        opacity: isDragging ? ".5" : 1,
-        //background: isHovered ? "gray" : "",
-    };
-
-    return (
-        <div
-            ref={setNodeRef}
-            id={props.id}
-            style={style}
-            {...attributes}
-            {...listeners}
-        >
-            {props.children}
-        </div>
-    );
+  return (
+    <div
+      ref={setNodeRef}
+      id={props.id}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
+      {props.children}
+    </div>
+  );
 };
 
 export default SortableItem;
