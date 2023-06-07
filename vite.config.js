a7dcb555b7329231f9459aa52f3f176105b3e248
Josh Bonnell <joshrbonnell@gmail.com>
2023-06-07 18:46:45 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
+  base: "/cbiv",
+  plugins: [react()],
 });
